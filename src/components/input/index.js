268c879607aa3067f85d0309b5eb516d4ed9e31c
thais <thaisventura@meirelesefreitas.adv.br>
2024-04-07 21:55:38 -0300

    feat: Create Input

--- src/components/input/index.js
+++ src/components/input/index.js
@@ -1,25 +1,67 @@
+import { icons } from "lucide-react";
 import styled from "styled-components";
 
-const InputForm = styled.input`
-    padding: 10px;
-    margin: 10px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    width: 100%;
-    box-sizing: border-box;
+const InputField = styled.input`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  appearance: none;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  align-items: center;
+  gap: 5px;
+  font-size: 1rem;
+  font-family: "Roboto", sans-serif;
+  color: #333;
 `;
 
+const InputContainer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  width: 100%;
+  height: ${({ height }) => height || "40px"};
+  padding: 5px 10px;
+  border: 1px solid #ccc;
+  border-radius: 10px;
 
+  gap: 5px;
+  background-color: #f2f4f7;
 
-const Input = ({ type, placeholder, value, onChange }) => {
-    return (
-        <InputForm
-        type={type}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        />
-    );
-    };
+  &:focus-within {
+    border-color: #007bff;
+    box-shadow: 0 0 0 0.07rem rgba(0, 123, 255, 0.25);
+    background-color: #fff;
+    color: #000;
+  }
+`;
 
+const IconLeft = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+
+  &:focus {
+    color: #007bff;
+  }
+`;
+const IconRight = styled.span`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+
+  &:focus {
+    color: #007bff;
+  }
+`;
+
+const Input = ({ iconLeft, iconRight, placeholder, type, ...props }) => (
+  <InputContainer>
+    {iconLeft && <IconLeft>{iconLeft}</IconLeft>}
+    <InputField placeholder={placeholder} type={type} {...props} />
+    {iconRight && <IconRight>{iconRight}</IconRight>}
+  </InputContainer>
+);
 export default Input;
